ff2c4527db2b09bf06c1906ca05dba1f8614402c
ligiadavilabozzi <ligia.bozzi@gmail.com>
2021-05-18 16:52:39 -0300

    create home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,14 @@
 import React from 'react'
-function Home(){
+
+function Home(props){
     return(
         <>
-        <h2>Últimos lançamentos</h2>
-        <div className="card">
-            <div className="thumb">
-                <img src="../imagens/capas/9788575228142.jpg" alt=""></img>
-            </div>
-            <div className="detalhes">
-                <h3>Padrões para Kubernetes</h3>
-                <p>O modo como...</p>
-                <a href="#">Leia mais &gt;</a>
-            </div>
-        </div>
-        <div className="card">
-            <div className="thumb">
-                <img src="./imagens/capas/9788575228074.jpg" alt=""></img>
-            </div>
-            <div className="detalhes">
-                <h3>Introdução ao Pentest - 2ª edição</h3>
-                <p>Introdução ao Pentest...</p>
-                <a href="#">Leia mais &gt;</a>
-            </div>
-        </div>
+            <h3>{props.tituloPagina}</h3>
+            <p>
+                ISBN: {props.isbn};
+                Título: {props.titulo};
+                Autor: {props.autor}
+            </p>
         </>
     )
 }
--- src/components/Principal.js
+++ src/components/Principal.js
@@ -2,9 +2,17 @@ import React from 'react'
 import Home from "./Home"
 
 function Principal(){
+    const isbn = "ISBN: 111111111"
+    const titulo = "React"
+    const autor = "Maujor"
+    const tituloPagina = "Últimos Lançamentos"
     return(
         <main className="principal">
-            <Home />
+            <Home
+            isbn={isbn}
+            titulo={titulo}
+            autor={autor}
+            tituloPagina={tituloPagina} />
         </main>
     )
 }
