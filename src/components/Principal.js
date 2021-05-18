dc8346b5c8cc151fd496dd2062d3e36c18706785
ligiadavilabozzi <ligia.bozzi@gmail.com>
2021-05-18 16:43:58 -0300

    create components

---
+++ src/components/Principal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Home from "./Home"
+
+function Principal(){
+    return(
+        <main className="principal">
+            <Home />
+        </main>
+    )
+}
+export default Principal
